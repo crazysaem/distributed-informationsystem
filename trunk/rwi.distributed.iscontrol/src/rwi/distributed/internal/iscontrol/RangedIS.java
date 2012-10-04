2b7e9a24f58f884c00996396d7923afd7f72bf95
martin.nathi@gmail.com <martin.nathi@gmail.com@bc29d898-8f9d-8b65-fcbc-63d1329752fb>
2012-10-04 08:15:25 +0000

    Added Master and RangedIS

---
+++ trunk/rwi.distributed.iscontrol/src/rwi/distributed/internal/iscontrol/RangedIS.java
@@ -0,0 +1,25 @@
+package rwi.distributed.internal.iscontrol;
+
+import rwi.dostributed.core.interfaces.server.IIS;
+
+public class RangedIS {
+	
+	private IIS infosystem;
+	private float minX, maxX, minY, maxY;
+
+	public RangedIS(float minX, float maxX, float minY, float maxY, IIS infosystem) {
+		this.minX = minX;
+		this.maxX = maxX;
+		this.minY = minY;
+		this.maxY = maxY;
+		this.infosystem = infosystem;
+	}
+
+	public IIS isInRange(float posX, float posY) {
+		if ((minX < posX && posX < maxX) && (minY < posY && posY < maxY)) {
+			return this.infosystem;
+		}
+		return null;
+	}
+
+}
