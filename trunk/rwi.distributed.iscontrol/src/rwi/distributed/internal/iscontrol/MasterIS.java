afcfbc2d58e686efd200433e85600288813c045d
tobiaskoenig1@googlemail.com <tobiaskoenig1@googlemail.com@bc29d898-8f9d-8b65-fcbc-63d1329752fb>
2012-10-04 08:17:26 +0000

    added idgen

--- trunk/rwi.distributed.iscontrol/src/rwi/distributed/internal/iscontrol/MasterIS.java
+++ trunk/rwi.distributed.iscontrol/src/rwi/distributed/internal/iscontrol/MasterIS.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import rwi.dostributed.core.interfaces.server.IIS;
+import rwi.informationsystem.internal.distributed.is.CentralInfoSysManager.IDGen;
 
 public class MasterIS {
 
 	ArrayList<RangedIS> infosystems;
 	HashMap<Integer, IIS> idMap;
 
+	private IDGen idgen;
+
+	public MasterIS() {
+		this.idgen = new IDGen();
+	}
+
 	public String register(int type, float posX, float posY) {
 
-		
 		IIS is = findIS(posX, posY);
 		return is.registerRWI_Object(id, type, posX, posY);
 
@@ -28,4 +34,29 @@ public class MasterIS {
 		return null;
 	}
 
+	private class IDGen {
+		private int id;
+		private ArrayList<Integer> freeids;
+
+		public IDGen() {
+			id = 0;
+			freeids = new ArrayList<Integer>();
+		}
+
+		public int nextId() {
+			if (freeids.size() == 0) {
+				return id++;
+			} else {
+				return freeids.remove(0);
+			}
+		}
+
+		public void removeId(int id) {
+			if (id == (this.id - 1)) {
+				this.id--;
+			} else {
+				this.freeids.add(id);
+			}
+		}
+	}
 }
