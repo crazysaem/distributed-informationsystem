8a3beb995a2be2f616e568e920ff367f50aab98a
tobiaskoenig1@googlemail.com <tobiaskoenig1@googlemail.com@bc29d898-8f9d-8b65-fcbc-63d1329752fb>
2012-10-06 08:24:44 +0000

    Init Servlets.

--- trunk/rwi.distributed.master.servlet/src/rwi/distributed/internal/master/servlet/ServletManager.java
+++ trunk/rwi.distributed.master.servlet/src/rwi/distributed/internal/master/servlet/ServletManager.java
@@ -1,8 +1,12 @@
 package rwi.distributed.internal.master.servlet;
 
-import org.eclipse.equinox.http.HttpService;
+import javax.servlet.ServletException;
+
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 
 import rwi.distributed.core.variables.RwiCommunication;
+import rwi.distributed.iscontrol.IMasterIs;
 
 public class ServletManager {
 
@@ -13,10 +17,24 @@ public class ServletManager {
 	}
 
 	protected void startup() {
-		String address = RwiCommunication.REGISTER_SERVLET;
-		RegisterServlet reg_servlet = new RegisterServlet(null);
-		reg_servlet.init();
+		initServlets(null);
+	}
 
-		http.registerServlet(address, reg_servlet, null, null);
+	private void initServlets(IMasterIs master) {
+		RegisterServlet reg_servlet = new RegisterServlet(master);
+		PositionServlet pos_servlet = new PositionServlet(master);
+		try {
+			// register Registration-Servlet
+			reg_servlet.init();
+			String address = RwiCommunication.REGISTER_SERVLET;
+			http.registerServlet(address, reg_servlet, null, null);
+			// register Position-Servlet
+			pos_servlet.init();
+			address = RwiCommunication.POSITION_SERVLET;
+			http.registerServlet(address, pos_servlet, null, null);
+		} catch (ServletException | NamespaceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
 }
